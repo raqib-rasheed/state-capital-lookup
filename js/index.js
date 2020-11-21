f006f8f7d59917f0f004de843d869b2fab65b824
Raqib Rasheed <raqib.abdulrasheed@outlook.com>
2020-11-21 18:30:49 +0530

    project completed

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+const searchState = async searchText => {
+    const res = await fetch("../data/states.json");
+    const states = await res.json();
+    
+    let matches = states.filter(state=>{
+        const regex = new RegExp(`${searchText}`,'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+    if(searchText.length === 0){
+        matches=[];
+        matchList.innerHTML = '';
+    }
+    outputHtml(matches);
+}
+
+//show result in html 
+const outputHtml = matches => {
+    if(matches.length>0){
+        const html = matches.map(match=>
+           `
+           <div class="card card-body mb-1">
+           <h4>${match.name} (${match.abbr})<span class="text-primary">${match.capital}</span></h4>
+           </div>
+           <small>lat : ${match.lat} / long:${match.long}</small>
+           ` 
+        
+        ).join("");
+        
+        matchList.innerHTML = html;
+    }
+}
+
+search.addEventListener('input',()=>searchState(search.value));
